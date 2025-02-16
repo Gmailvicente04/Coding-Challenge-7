aa63a655ea5065e169538785641c3ad7075668e3
Gmailvicente04 <vicente.oswald@gmail.com>
2025-02-16 17:10:24 -0500

    "Task 6 - Filtering High-Value Transactions."

--- cc_7.js
+++ cc_7.js
@@ -67,11 +67,11 @@ function calculateLoanInterest(principal, rate, years) {
   console.log(calculateLoanInterest(5000, 0.07, 5)); // Expected output: "Total Interest: $1750.00"
   
   // Task 6: Higher-Order Functions
-  let transactions = [500, 1200, 3000, 800, 2200];
+  let transactions = [900, 1200, 3000, 800, 2200];
   function filterHighValueTransactions(transactions, filterFunction) {
     return transactions.filter(filterFunction);
   }
-  
+
   // Test Data
   console.log(filterHighValueTransactions(transactions, amount => amount > 1000)); // Expected output: [1200, 3000, 2200]
   
